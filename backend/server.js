53914cb8fe2a394b11a7f13a72dd61dba3c7f599
kdaehun00 <kdaehun00@naver.com>
2025-07-23 20:35:31 +0900

    feat: init setting

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const routes = require('./routes');
 
 const app = express();
 const server = http.createServer(app);
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // trust proxy 설정 추가
 app.set('trust proxy', 1);
