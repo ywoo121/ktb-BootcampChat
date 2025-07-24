7b6dbc68d959208e15f3345e8b2775734201201b
Minseok Kim <mskim.glenn@gmail.com>
2025-07-24 17:00:45 +0900

    feat: CORS 도메인 추가

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,9 @@ const corsOptions = {
     'http://0.0.0.0:3000',
     'https://0.0.0.0:3000',
     'https://chat.goorm-ktb-006.goorm.team'
+    'http://chat.goorm-ktb-006.goorm.team'
+    'https://api.chat.goorm-ktb-006.goorm.team'
+    'http://api.chat.goorm-ktb-006.goorm.team'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
